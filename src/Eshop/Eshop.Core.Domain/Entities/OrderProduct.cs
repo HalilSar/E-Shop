241412e8b36e2affc96eedc7198a326b878e672c
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 00:42:29 +0300

    OrderProduct added.

---
+++ src/Eshop/Eshop.Core.Domain/Entities/OrderProduct.cs
@@ -0,0 +1,12 @@
+﻿using Eshop.Core.Domain.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Eshop.Core.Domain.Entities
+{
+    public class OrderProduct : BaseEntity
+    {
+    }
+}
