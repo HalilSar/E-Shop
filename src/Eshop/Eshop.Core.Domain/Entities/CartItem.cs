a61bb6e553a2bf22715d8b5cfe70e18c06b6209c
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 00:41:02 +0300

    Entities change.

--- src/Eshop/Eshop.Core.Domain/Entities/CartItem.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/CartItem.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Eshop.Core.Domain.Abstracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Eshop.Core.Domain.Entities
 {
-    class CartItem
+    public class CartItem : BaseEntity 
     {
+
     }
 }
--- src/Eshop/Eshop.Core.Domain/Entities/Category.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Eshop.Core.Domain.Abstracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Eshop.Core.Domain.Entities
 {
-    class Category
+    public class Category:BaseEntity
     {
     }
 }
