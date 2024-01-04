b9538b8130b12a1dc90b8e17a98f3068cb2d5aa3
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 18:59:56 +0300

    image,description,detail added to Product.

--- src/Eshop/Eshop.Core.Domain/Entities/Product.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/Product.cs
@@ -10,6 +10,9 @@ namespace Eshop.Core.Domain.Entities
     public class Product:BaseEntity
     {
         public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Detail { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
     }
