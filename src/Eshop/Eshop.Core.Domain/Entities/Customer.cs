5d4f5f7cb47bf72a49cedd6dacd33a32bc382f27
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 19:04:47 +0300

    firstname, lastname, email, password added to customer.

--- src/Eshop/Eshop.Core.Domain/Entities/Customer.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/Customer.cs
@@ -9,5 +9,9 @@ namespace Eshop.Core.Domain.Entities
 {
     public class Customer:BaseEntity
     {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
