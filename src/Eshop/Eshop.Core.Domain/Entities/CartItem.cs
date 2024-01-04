5737125b919a38101ebc76d03ed7870a95c79a1c
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 19:11:37 +0300

    customerID,customer,product,productID,amount,subtotal added to cartItem class.

--- src/Eshop/Eshop.Core.Domain/Entities/CartItem.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/CartItem.cs
@@ -8,6 +8,11 @@ namespace Eshop.Core.Domain.Entities
 {
     public class CartItem : BaseEntity 
     {
-
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public double SubTotal { get; set; }
     }
 }
