0eb634b6eb1656c45243f242d74d3d7585e97cc2
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 19:51:43 +0300

    OrderProduct props added.

--- src/Eshop/Eshop.Core.Domain/Entities/OrderProduct.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/OrderProduct.cs
@@ -8,5 +8,13 @@ namespace Eshop.Core.Domain.Entities
 {
     public class OrderProduct : BaseEntity
     {
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+        public double SubTotal { get; set; }
+
     }
 }
