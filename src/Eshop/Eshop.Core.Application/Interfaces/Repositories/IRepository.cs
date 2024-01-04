0c2a1f96659c9cb5f3fac91b9ce22edd16410831
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 22:25:21 +0300

    IRepository added.

---
+++ src/Eshop/Eshop.Core.Application/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Eshop.Core.Domain.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Interfaces.Repositories
+{
+    public interface IRepository<T> where T:BaseEntity, new()
+    {
+    }
+}
