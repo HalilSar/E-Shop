2045b256bfc1cdefec00c3bc1d865dd76d18345f
halil37 <hllibrahimsartitas@gmail.com>
2024-01-04 20:18:50 +0300

    Customer prop added to OrderProduct class.

--- src/Eshop/Eshop.Core.Domain/Entities/OrderProduct.cs
+++ src/Eshop/Eshop.Core.Domain/Entities/OrderProduct.cs
@@ -3,7 +3,8 @@ namespace Eshop.Core.Domain.Entities
 {
     public class OrderProduct : BaseEntity
     {
-
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
         public int OrderId { get; set; }
         public Order Order { get; set; }
         public int ProductId { get; set; }
