6c4a8c5917a2778bb1ac1cb49bf7701cfc23a371
halil37 <hllibrahimsartitas@gmail.com>
2024-01-05 19:28:27 +0300

    DbSets added.

--- src/Eshop/Eshop.Core.Application/Interfaces/Context/IApplication.cs
+++ src/Eshop/Eshop.Core.Application/Interfaces/Context/IApplication.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Eshop.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Eshop.Core.Application.Interfaces.Context
 {
     public interface IApplication
     {
+        public DbSet<Customer>       Customers     { get; set; }
+        public DbSet<Category>       Categories    { get; set; }
+        public DbSet<Product>        Products      { get; set; }
+        public DbSet<Order>          Orders        { get; set; }
+        public DbSet<OrderProduct>   OrderProducts { get; set; }
+        public DbSet<CartItem>       CartItems     { get; set; }
     }
 }
