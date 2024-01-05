3ad1b1d814c34702e58eca8c83a833d687de89d5
halil37 <hllibrahimsartitas@gmail.com>
2024-01-05 19:49:04 +0300

    method signs of irepository at irepository interface.

--- src/Eshop/Eshop.Core.Application/Interfaces/Repositories/IRepository.cs
+++ src/Eshop/Eshop.Core.Application/Interfaces/Repositories/IRepository.cs
@@ -9,5 +9,10 @@ namespace Eshop.Core.Application.Interfaces.Repositories
 {
     public interface IRepository<T> where T:BaseEntity, new()
     {
+
+        Task Add(T entity);
+        Task Delete(T entity);
+        Task<List<T>> Get();
+        Task<T> GetById(int id);
     }
 }
