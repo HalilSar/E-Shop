709463cd19103b0cf5fea6b237452243b27c91bf
halil37 <hllibrahimsartitas@gmail.com>
2024-01-06 22:32:29 +0300

    members of getproductresponse added in getproductresponse

--- src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductResponse.cs
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductResponse.cs
@@ -8,5 +8,9 @@ namespace Eshop.Core.Application.Features.Queries.GetProduct
 {
     public class GetProductResponse
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Detail { get; set; }
     }
 }
