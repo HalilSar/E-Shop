39510749a5151a65df97b1ec95ed10311b66fa4c
halil37 <hllibrahimsartitas@gmail.com>
2024-01-06 23:00:30 +0300

    getbyidproductresponse added.

--- src/Eshop/Eshop.Core.Application/Features/Queries/GetByIdProduct/GetByIdResponse.cs
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetByIdProduct/GetByIdResponse.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eshop.Core.Application.Features.Queries.GetProduct
+namespace Eshop.Core.Application.Features.Queries.GetByIdProduct
 {
-    public class GetProductResponse
+    public  class GetByIdResponse
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public string Detail { get; set; }
+        public int CategoryId { get; set; }
     }
 }
