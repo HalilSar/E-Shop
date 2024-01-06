df94d7b8220ec1730615eb6e43e0ce4fcce24080
halil37 <hllibrahimsartitas@gmail.com>
2024-01-06 22:49:50 +0300

    GetProductHandler added

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetProduct
+{
+    public class GetProductHandler : IRequestHandler<GetProductRequest, List<GetProductResponse>>
+    {
+
+        IProductRepository _productRepository;
+        IMapper _mapper;
+        public GetProductHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+
+        }
+        public async Task<List<GetProductResponse>> Handle(GetProductRequest request, CancellationToken cancellationToken)
+        {
+            var products =  _productRepository.Get();
+            return  _mapper.Map<List<GetProductResponse>>(products);
+           
+        }
+    }
+}
--- src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductRequest.cs
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductRequest.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
 
 namespace Eshop.Core.Application.Features.Queries.GetProduct
 {
-    public class GetProductRequest
+    public class GetProductRequest : IRequest<List<GetProductResponse>>
     {
     }
 }
