a6c941a35dddbe65771bc905b4025caa3bc6c3da
halil37 <hllibrahimsartitas@gmail.com>
2024-01-06 23:21:55 +0300

    getcategoryrequest and getcategoryresponse added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetCategory/GetCategoryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetCategory
+{
+     public  class GetCategoryResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
