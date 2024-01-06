8a18a21ff29d82302ddec588ef05b939fc74377e
halil37 <hllibrahimsartitas@gmail.com>
2024-01-06 23:27:25 +0300

    GetCategoryHandler added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetCategory/GetCategoryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetCategory
+{
+    public class GetCategoryHandler : IRequestHandler<GetCategoryRequest, List<GetCategoryResponse>>
+    {
+        IMapper _mapper;
+        ICategoryRepository _categoryRepository;
+        public GetCategoryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public  async Task<List<GetCategoryResponse>> Handle(GetCategoryRequest request, CancellationToken cancellationToken)
+        {
+            var categories = _categoryRepository.Get();
+            return _mapper.Map<List<GetCategoryResponse>>(categories);
+        }
+    }
+}
