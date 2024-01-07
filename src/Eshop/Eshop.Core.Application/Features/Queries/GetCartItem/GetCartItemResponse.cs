a674b830a3a4c17890cd41014ecc1a98abdb5c6f
halil37 <hllibrahimsartitas@gmail.com>
2024-01-07 20:00:43 +0300

    GetCartItemResponse added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetCartItem/GetCartItemResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetCartItem
+{
+    public  class GetCartItemResponse
+    {
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+        public double SubTotal { get; set; }
+    }
+}
