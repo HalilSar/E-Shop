00f6417b02069153892d3a7fb966624ca2d35f3b
halil37 <hllibrahimsartitas@gmail.com>
2024-01-07 19:48:11 +0300

    GetByIdOrderProductHandler added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetByIdOrderProduct/GetByIdOrderProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetByIdOrderProduct
+{
+    public  class GetByIdOrderProductHandler : IRequestHandler<GetByIdOrderProductRequest,GetByIdOrderProductResponse>
+    {
+        IMapper _mapper;
+        IOrderProductRepository _orderProductRepository;
+        public GetByIdOrderProductHandler(IMapper mapper, IOrderProductRepository orderProductRepository)
+        {
+            _mapper = mapper;
+            _orderProductRepository = orderProductRepository;
+        }
+        public async Task<GetByIdOrderProductResponse> Handle(GetByIdOrderProductRequest request, CancellationToken cancellationToken)
+        {
+            var orderProduct = _orderProductRepository.GetById(request.Id);
+            return _mapper.Map<GetByIdOrderProductResponse>(orderProduct);
+        }
+    }
+}
