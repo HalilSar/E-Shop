6720506933a0d56bb90da9351fe44f138d131fde
halil37 <hllibrahimsartitas@gmail.com>
2024-01-09 23:36:34 +0300

    getbycustomeridordershandler added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetByCustomerIdOrders/GetByCustomerIdOrdersHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetByCustomerIdOrders
+{
+   public class GetByCustomerIdOrdersHandler : IRequestHandler<GetByCustomerIdOrdersRequest, List<GetByCustomerIdOrdersResponse>>
+    {
+        IMapper _mapper;
+        IOrderRepository _orderRepository;
+        public GetByCustomerIdOrdersHandler(IMapper mapper, IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<List<GetByCustomerIdOrdersResponse>> Handle(GetByCustomerIdOrdersRequest request, CancellationToken cancellationToken)
+        {
+           var order = _orderRepository.GetByCustomerId(request.CategoryId);
+           return _mapper.Map<List<GetByCustomerIdOrdersResponse>>(order);
+        }
+    }
+}
