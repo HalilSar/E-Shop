cc1509ae09c4eb9fcd4fceba1f584df05ce7d972
halil37 <hllibrahimsartitas@gmail.com>
2024-01-10 23:05:02 +0300

    deletecartitemreq added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Commands/DeleteCartItem/DeleteCartItemRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Commands.DeleteteCartItem
+{
+     public class DeleteCartItemRequest : IRequest<CreateCartItemResponse>
+    {
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+        public double SubTotal { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
