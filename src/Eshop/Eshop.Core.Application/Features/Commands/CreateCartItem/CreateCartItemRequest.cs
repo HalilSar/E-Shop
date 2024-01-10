960af042ce71750e9737eaf51ad2326a2ec51e49
halil37 <hllibrahimsartitas@gmail.com>
2024-01-10 22:49:34 +0300

    createcaritemrequest added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Commands/CreateCartItem/CreateCartItemRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Commands.CreateCartItem
+{
+     public class CreateCartItemRequest
+    {
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+        public double SubTotal { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
