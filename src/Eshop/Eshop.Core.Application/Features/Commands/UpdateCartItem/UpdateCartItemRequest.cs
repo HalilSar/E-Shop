6472b9ecfb2d438878b0bf56e4d36f295b29997a
halil37 <hllibrahimsartitas@gmail.com>
2024-01-11 21:40:27 +0300

    updatecartitemrequest added.

--- src/Eshop/Eshop.Core.Application/Features/Commands/UpdateCartItem/UpdateCartItemRequest.cs
+++ src/Eshop/Eshop.Core.Application/Features/Commands/UpdateCartItem/UpdateCartItemRequest.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eshop.Core.Application.Features.Commands.CreateCartItem
+namespace Eshop.Core.Application.Features.Commands.UpdateCartItem
 {
-     public class CreateCartItemRequest:IRequest<CreateCartItemResponse>
+     public class UpdateCartItemRequest : IRequest<UpdateCartItemResponse>
     {
         public int CustomerId { get; set; }
         public int ProductId { get; set; }
