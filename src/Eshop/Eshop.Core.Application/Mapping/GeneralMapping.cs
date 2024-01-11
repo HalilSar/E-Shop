de40b6cdcc8e1a22a522f88af32c8ea738c4bfec
halil37 <hllibrahimsartitas@gmail.com>
2024-01-11 21:33:32 +0300

    general mapping added.

---
+++ src/Eshop/Eshop.Core.Application/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+           
+        }
+    }
+}
