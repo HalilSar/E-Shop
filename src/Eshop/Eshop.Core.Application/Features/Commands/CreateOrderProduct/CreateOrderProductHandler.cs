bd80c9dcb7028ed90b53e581b07eefc25ca7460c
halil37 <hllibrahimsartitas@gmail.com>
2024-01-11 21:22:32 +0300

    createorderproducthandler added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Commands/CreateOrderProduct/CreateOrderProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using Eshop.Core.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Commands.CreateOrderProduct
+{
+   public class CreateOrderProductHandler : IRequestHandler<CreateOrderProductRequest,CreateOrderProductResponse>
+    {
+        IOrderProductRepository _orderProductRepository;
+        IMapper _mapper;
+        public CreateOrderProductHandler(IOrderProductRepository orderProductRepository, IMapper mapper)
+        {
+            _orderProductRepository = orderProductRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateOrderProductResponse> Handle(CreateOrderProductRequest request, CancellationToken cancellationToken)
+        {
+            var order = _mapper.Map<OrderProduct>(request);
+           await _orderProductRepository.Add(order);
+            return new CreateOrderProductResponse { Success = "OrderProduct added." };
+        }
+    }
+}
