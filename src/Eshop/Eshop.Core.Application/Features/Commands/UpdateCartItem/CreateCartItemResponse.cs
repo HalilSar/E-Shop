cc6c472cab7ee8df60ed70f412b1dbea5951c5f8
halil37 <hllibrahimsartitas@gmail.com>
2024-01-11 21:38:43 +0300

    updatecartitemres added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Commands/UpdateCartItem/CreateCartItemResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Commands.UpdateCartItem
+{
+    public class UpdateCartItemResponse
+    {
+        public string Success { get; set; }
+    }
+}
