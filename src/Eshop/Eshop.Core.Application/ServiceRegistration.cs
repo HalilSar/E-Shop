d6fa31258f0173d6413beffc465325ee422f03a1
halil37 <hllibrahimsartitas@gmail.com>
2024-01-13 23:57:19 +0300

    AddAutoMapper added to ServiceRegistration.

--- src/Eshop/Eshop.Core.Application/ServiceRegistration.cs
+++ src/Eshop/Eshop.Core.Application/ServiceRegistration.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,9 @@ namespace Eshop.Core.Application
 {
     static public class ServiceRegistration
     {
-        
+        static public void AddApplicationServices(this IServiceCollection service)
+        {
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
     }
 }
