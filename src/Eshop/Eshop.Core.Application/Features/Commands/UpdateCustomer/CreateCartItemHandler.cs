89de63b2ac23d5a5d101011c79bd02fe122c9d77
halil37 <hllibrahimsartitas@gmail.com>
2024-01-13 22:48:56 +0300

    UpdateCustomerResponse added.

---
+++ src/Eshop/Eshop.Core.Application/Features/Commands/UpdateCustomer/CreateCartItemHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using Eshop.Core.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Commands.CreateCustomer
+{
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomerRequest, CreateCustomerResponse>
+    {
+        ICustomerRepository _customerRepository;
+        IMapper _mapper;
+        public CreateCustomerHandler(ICustomerRepository customerRepository, IMapper mapper)
+        {
+            _customerRepository = customerRepository;
+            _mapper = mapper;
+    }
+
+        public async Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+        {
+           var customer = _mapper.Map<Customer>(request);
+           await _customerRepository.Add(customer);
+            return new CreateCustomerResponse { Success = "Başarılı" };
+        }
+    }
+}
