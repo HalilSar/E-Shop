4c98c9a9815502557364bd5d5da2adf1590bf31b
halil37 <hllibrahimsartitas@gmail.com>
2024-01-15 01:43:23 +0300

    OnConfigurring created.

--- src/Eshop/Eshop.Infrastructure.Persistence/DbContexts/ApplicationDbContext.cs
+++ src/Eshop/Eshop.Infrastructure.Persistence/DbContexts/ApplicationDbContext.cs
@@ -6,20 +6,19 @@ using System.Threading.Tasks;
 using Eshop.Core.Application.Interfaces.Context;
 using Microsoft.EntityFrameworkCore;
 using Eshop.Core.Domain.Entities;
-
 namespace Eshop.Infrastructure.Persistence.DbContexts
 {
     public  class ApplicationDbContext:DbContext,IApplicationDbContext
     {
-       // IConfiguration configuration;
+        // IConfiguration configuration;
         //public ApplicationDbContext()
         //{
 
         //}
-        //public  void OnConfiguring(DbContextOptions<ApplicationDbContext> options)
-        //{
-        //    options.D
-        //}
+        public void OnConfiguring(DbContextOptionsBuilder<ApplicationDbContext> options)
+        {
+            options.UseSqlServer("Your Connection String");
+        }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
