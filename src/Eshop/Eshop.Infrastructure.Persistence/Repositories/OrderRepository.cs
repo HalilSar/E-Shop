09ec610c7b5e0aad42cabea34b8426f16d6b62d3
halil37 <hllibrahimsartitas@gmail.com>
2024-01-15 22:22:42 +0300

    orderrepository added.

---
+++ src/Eshop/Eshop.Infrastructure.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using Eshop.Core.Application.Interfaces.Repositories;
+using Eshop.Core.Domain.Entities;
+using Eshop.Infrastructure.Persistence.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Infrastructure.Persistence.Repositories
+{
+    public class OrderRepository:Repository<Order>,IOrderRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public OrderRepository(ApplicationDbContext applicationDbContext):base(applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public List<Order> GetByCustomerId(int id)
+        {
+           return  _applicationDbContext.Set<Order>().Where(x => x.CustomerId == id).ToList();
+
+        }
+
+        public Order GetByCustomerIdOrder(int id, int customerId)
+        {
+            return _applicationDbContext.Set<Order>().Where(x => x.Id == id && x.CustomerId == customerId).FirstOrDefault();
+        }
+    }
+}
