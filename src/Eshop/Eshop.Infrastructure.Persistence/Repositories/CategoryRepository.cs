c5a17cd4375f7e2e0ad2bdc8cc7df7b69e064217
halil37 <hllibrahimsartitas@gmail.com>
2024-01-15 22:01:20 +0300

    categoryrepository added.

---
+++ src/Eshop/Eshop.Infrastructure.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Eshop.Core.Application.Interfaces.Repositories;
+using Eshop.Core.Domain.Entities;
+using Eshop.Infrastructure.Persistence.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Infrastructure.Persistence.Repositories
+{
+    public  class CategoryRepository:Repository<Category>,ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext applicationDbContext):base(applicationDbContext)
+        {
+
+        }
+    }
+}
