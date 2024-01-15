d1009d6db02582a9b627e30856344c31b013f204
halil37 <hllibrahimsartitas@gmail.com>
2024-01-15 22:32:42 +0300

    cartitemrepos added.

---
+++ src/Eshop/Eshop.Infrastructure.Persistence/Repositories/CartItemRepository.cs
@@ -0,0 +1,25 @@
+﻿using Eshop.Core.Application.Interfaces.Repositories;
+using Eshop.Core.Domain.Entities;
+using Eshop.Infrastructure.Persistence.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Infrastructure.Persistence.Repositories
+{
+    public  class CartItemRepository:Repository<CartItem>,ICartItemRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public CartItemRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public List<CartItem> GetByCustormerId(int id)
+        {
+            return _applicationDbContext.Set<CartItem>().Where(x => x.CustomerId == id).ToList();
+        }
+    }
+}
