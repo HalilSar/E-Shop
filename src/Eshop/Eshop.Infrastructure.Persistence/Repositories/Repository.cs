5caa5f70a1279167bb8e3b6ac13b378c3badf8e4
halil37 <hllibrahimsartitas@gmail.com>
2024-01-15 21:08:45 +0300

    repository added.

---
+++ src/Eshop/Eshop.Infrastructure.Persistence/Repositories/Repository.cs
@@ -0,0 +1,38 @@
+﻿using Eshop.Core.Application.Interfaces.Repositories;
+using Eshop.Core.Domain.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Infrastructure.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity, new()
+    {
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
