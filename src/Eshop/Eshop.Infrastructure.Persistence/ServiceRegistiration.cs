e02319a08b7ed8d594994f9b5b56ac6eb3cbab12
halil37 <hllibrahimsartitas@gmail.com>
2024-01-17 21:34:36 +0300

    dbcontext added in servicesreg.

--- src/Eshop/Eshop.Infrastructure.Persistence/ServiceRegistiration.cs
+++ src/Eshop/Eshop.Infrastructure.Persistence/ServiceRegistiration.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Eshop.Infrastructure.Persistence.DbContexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Eshop.Infrastructure.Persistence
 {
     static public class ServiceRegistiration
     {
+        public static  void AddPersistenceServices(this IServiceCollection service)
+        {
+            service.AddDbContext<ApplicationDbContext>();
+          
+        }
     }
 }
