f3463ede3efcd6fb506ddbc54e7154966536af2f
halil37 <hllibrahimsartitas@gmail.com>
2024-01-18 20:49:09 +0300

    application services added inf/servicereg.

---
+++ src/Eshop/Eshop.Infrastructure.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Eshop.Core.Application;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Infrastructure.Infrastructure
+{
+    static public class ServiceRegistration
+    {
+        static public void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddApplicationServices();
+        }
+    }
+}
