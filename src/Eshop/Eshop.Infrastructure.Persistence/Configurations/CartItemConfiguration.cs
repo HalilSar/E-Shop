64f31080eb2bc55447d672429d3a06aabbbbf8aa
halil37 <hllibrahimsartitas@gmail.com>
2024-01-19 21:53:41 +0300

    cartitemconfiguration added to persis/conf.

---
+++ src/Eshop/Eshop.Infrastructure.Persistence/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Eshop.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Infrastructure.Persistence.Configurations
+{
+    public  class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.HasOne<Customer>().WithMany().HasForeignKey(i => i.CustomerId);
+            builder.HasOne<Product>().WithMany().HasForeignKey(i => i.ProductId);
+
+        }
+    }
+}
