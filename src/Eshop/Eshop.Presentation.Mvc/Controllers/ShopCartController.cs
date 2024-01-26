348f6dbb3d126de9329c7b74a53c34492b2dccd3
halil37 <hllibrahimsartitas@gmail.com>
2024-01-26 15:03:56 +0300

    shopcartcontroller added.

---
+++ src/Eshop/Eshop.Presentation.Mvc/Controllers/ShopCartController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Presentation.Mvc.Controllers
+{
+    public class ShopCartController : Controller
+    {
+        private readonly ILogger<ShopCartController> _logger;
+        private readonly IMediator _mediatR;
+
+        public ShopCartController(ILogger<ShopCartController> logger, IMediator mediatR)
+        {
+            _logger = logger;
+            _mediatR = mediatR;
+        }
+    }
+}
