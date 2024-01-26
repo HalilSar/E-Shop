54d782a5b5eafdfd1687c907d9f1b95ca9734020
halil37 <hllibrahimsartitas@gmail.com>
2024-01-26 15:41:14 +0300

    changes.

---
+++ src/Eshop/Eshop.Presentation.Mvc/Controllers/ShopCartController - Copy.cs
@@ -0,0 +1,57 @@
+﻿using Eshop.Core.Application.Features.Commands.CreateCartItem;
+using Eshop.Core.Application.Features.Commands.DeleteCartItem;
+using Eshop.Core.Application.Features.Commands.UpdateCartItem;
+using Eshop.Core.Application.Features.Queries.GetByCustormerIdCartItem;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Presentation.Mvc.Controllers
+{
+    public class ShopCartController : Controller
+    {
+        private readonly ILogger<ShopCartController> _logger;
+        private readonly IMediator _mediatR;
+
+        public ShopCartController(ILogger<ShopCartController> logger, IMediator mediatR)
+        {
+            _logger = logger;
+            _mediatR = mediatR;
+        }
+
+        public IActionResult Cart(GetByCustormerIdCartItemRequest request)
+        {        // Burayı güncelle. customerId sini nasıl user.id sistemine katarım.  
+            return View(_mediatR.Send(request));
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(CreateCartItemRequest request)
+        {         
+            return View(_mediatR.Send(request));
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(UpdateCartItemRequest request)
+        {
+            return View(_mediatR.Send(request));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DeleteCartItemRequest request)
+        {
+            return View(_mediatR.Send(request));
+        }
+
+    }
+}
