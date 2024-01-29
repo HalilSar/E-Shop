5054c1e1587c222d80c70f7ab3a7a181c904dfa1
halil37 <hllibrahimsartitas@gmail.com>
2024-01-29 17:55:32 +0300

    categoryviewcomponent added to viewcomponents.

---
+++ src/Eshop/Eshop.Presentation.Mvc/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Presentation.Mvc.ViewComponents
+{
+    public class CategoryViewComponent
+    {
+    }
+}
