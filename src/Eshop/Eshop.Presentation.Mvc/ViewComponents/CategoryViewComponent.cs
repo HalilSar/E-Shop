1f218eb129947a93e53008f5dea24d06c83a3877
halil37 <hllibrahimsartitas@gmail.com>
2024-01-29 18:02:49 +0300

    invoke method added to CategoryViewComponent

--- src/Eshop/Eshop.Presentation.Mvc/ViewComponents/CategoryViewComponent.cs
+++ src/Eshop/Eshop.Presentation.Mvc/ViewComponents/CategoryViewComponent.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Eshop.Core.Application.Features.Queries.GetCategory;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Eshop.Presentation.Mvc.ViewComponents
 {
-    public class CategoryViewComponent
+    public class CategoryViewComponent:ViewComponent
     {
+        private readonly IMediator _meditR;
+        public CategoryViewComponent(IMediator meditR)
+        {
+            _meditR = meditR;
+        }
+
+        public async Task<List<GetCategoryResponse>> Invoke(GetCategoryRequest request)
+        {
+           return  await _meditR.Send(request);
+        }
     }
 }
