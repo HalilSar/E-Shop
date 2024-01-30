eed21b73cfc837659a494adfe622a764f09d6a93
halil37 <hllibrahimsartitas@gmail.com>
2024-01-30 16:23:57 +0300

    updated to crateorderreq.

--- src/Eshop/Eshop.Core.Application/Features/Commands/CreateOrder/CreateOrderHandler.cs
+++ src/Eshop/Eshop.Core.Application/Features/Commands/CreateOrder/CreateOrderHandler.cs
@@ -15,8 +15,9 @@ namespace Eshop.Core.Application.Features.Commands.CreateOrder
     {
         IOrderRepository _orderRepository;
         IOrderProductRepository _orderProductRepository;
+        ICartItemRepository _cartItemRepository;
         IMapper _mapper;
-        public CreateOrderHandler(IOrderRepository orderRepository, IMapper mapper)
+        public CreateOrderHandler(IOrderRepository orderRepository, IOrderProductRepository orderProductRepository, ICartItemRepository cartItemRepository, IMapper mapper)
         {
             _orderRepository = orderRepository;
             _mapper = mapper;
@@ -25,8 +26,11 @@ namespace Eshop.Core.Application.Features.Commands.CreateOrder
         public async Task<CreateOrderResponse> Handle(CreateOrderRequest request, CancellationToken cancellationToken)
         {
             // var carts =_applicationDbContext.CartItems.Where(i=>i.CustomerId==customerId);
+
             var order = _mapper.Map<Order>(request);
            await _orderRepository.Add(order);
+         //   var carts=_cartItemRepository.GetByCustormerId(request.cust)
+          //  _orderProductRepository.CreateOrderProducts(carts)
             return new CreateOrderResponse { Success = "Order eklendi" };
         }
     }
