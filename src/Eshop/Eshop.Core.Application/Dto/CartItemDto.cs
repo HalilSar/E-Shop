e5300c0848839f3b608a50a8caff7923a4928825
halil37 <hllibrahimsartitas@gmail.com>
2024-01-30 16:44:22 +0300

    crea..orderpro..req changed.

--- src/Eshop/Eshop.Core.Application/Dto/CartItemDto.cs
+++ src/Eshop/Eshop.Core.Application/Dto/CartItemDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Eshop.Core.Application.Dto
+﻿namespace Eshop.Core.Application.Dto
 {
     public class CartItemDto
     {
