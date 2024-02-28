152bc4d9cd142f3af11ab0ae1f0ab3d5922cabcc
halil37 <hllibrahimsartitas@gmail.com>
2024-02-28 18:49:21 +0300

    index and getproductresponse changed.

--- src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductResponse.cs
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetProduct/GetProductResponse.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eshop.Core.Application.Features.Queries
+namespace Eshop.Core.Application.Features.Queries.GetProduct
 {
     public class GetProductResponse
     {
