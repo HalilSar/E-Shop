b45cf6aaaefbca7528cd3dde31e21d108de4d864
halil37 <hllibrahimsartitas@gmail.com>
2024-03-06 22:06:08 +0300

    configurations modified.

--- src/Eshop/Eshop.Infrastructure.Persistence/Configurations/OrderConfiguration.cs
+++ src/Eshop/Eshop.Infrastructure.Persistence/Configurations/OrderConfiguration.cs
@@ -14,7 +14,7 @@ namespace Eshop.Infrastructure.Persistence.Configurations
             builder.Property(i => i.PhoneNumber).IsRequired().HasMaxLength(20);
             builder.Property(i => i.Adress).IsRequired().HasMaxLength(20);
             builder.Property(i => i.Total).IsRequired().HasMaxLength(20);
-            builder.HasOne<Customer>().WithMany().HasForeignKey(i => i.CustomerId).HasForeignKey("CustomerId");
+            builder.HasOne<Customer>().WithMany().HasForeignKey(i => i.CustomerId).HasConstraintName("CustomerId");
 
         }
     }
--- src/Eshop/Eshop.Infrastructure.Persistence/Configurations/OrderProductConfiguration.cs
+++ src/Eshop/Eshop.Infrastructure.Persistence/Configurations/OrderProductConfiguration.cs
@@ -9,9 +9,9 @@ namespace Eshop.Infrastructure.Persistence.Configurations
         public void Configure(EntityTypeBuilder<OrderProduct> builder)
         {
             builder.HasKey(u => u.Id);
-            builder.HasOne<Customer>().WithMany().HasForeignKey(i => i.CustomerId).HasForeignKey("CustomerId");
-            builder.HasOne<Order>().WithMany().HasForeignKey(i => i.OrderId).OnDelete(DeleteBehavior.Restrict).HasForeignKey("OrderId");
-            builder.HasOne<Product>().WithMany().HasForeignKey(i => i.ProductId).HasForeignKey("ProductId");
+            builder.HasOne<Customer>().WithMany().HasForeignKey(i => i.CustomerId).HasConstraintName("CustomerId");
+            builder.HasOne<Order>().WithMany().HasForeignKey(i => i.OrderId).OnDelete(DeleteBehavior.Restrict).HasConstraintName("OrderId");
+            builder.HasOne<Product>().WithMany().HasForeignKey(i => i.ProductId).HasConstraintName("ProductId");
         }
     }
 }
