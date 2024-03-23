da7288b257c7f24efda2966b2fedb3e382065ec0
halil37 <hllibrahimsartitas@gmail.com>
2024-03-23 23:45:46 +0300

    GetByEmailPasswordCustomerStatus added in queries.

---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetByEmailPasswordCustomerStatus/GetByIdCustomerHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Eshop.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetByIdCustomer
+{
+    public class GetByIdCustomerHandler : IRequestHandler< GetByIdCustomerRequest,GetByIdCustomerResponse>
+    {
+        IMapper _mapper;
+        ICustomerRepository _customerRepository;
+        public GetByIdCustomerHandler(IMapper mapper, ICustomerRepository customerRepository)
+        {
+            _mapper = mapper;
+            _customerRepository = customerRepository;
+        }
+        public async Task<GetByIdCustomerResponse> Handle(GetByIdCustomerRequest request, CancellationToken cancellationToken)
+        {
+            var customer = await _customerRepository.GetById(request.Id);
+            return _mapper.Map<GetByIdCustomerResponse>(customer); 
+        }
+    }
+}
---
+++ src/Eshop/Eshop.Core.Application/Features/Queries/GetByEmailPasswordCustomerStatus/GetByIdCustomerResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Application.Features.Queries.GetByIdCustomer
+{
+    public class GetByEmailPasswordCustomerStatusResponse
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
