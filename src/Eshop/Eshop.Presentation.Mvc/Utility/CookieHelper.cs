db10ec9f9a96c6cdeb3555008300daa5a23951a7
halil37 <hllibrahimsartitas@gmail.com>
2024-03-26 23:28:55 +0300

    CookieHelper added and modified at cookiehelper in presentation layer.

---
+++ src/Eshop/Eshop.Presentation.Mvc/Utility/CookieHelper.cs
@@ -0,0 +1,20 @@
+﻿using Eshop.Core.Application.Features.Queries.GetByEmailPasswordCustomerStatus;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.Presentation.Mvc.Utility
+{
+    public static class CookieHelper
+    {
+        
+        public static void SetUserInfo(HttpContext context, GetByEmailPasswordCustomerStatusRequest request)
+        {
+            var options = new CookieOptions { Expires = DateTime.Now.AddDays(7), HttpOnly = true };
+            context.Response.Cookies.Append("UserInfo", $"{request.Email}|{request.Password}",options);
+        }
+    }
+}
