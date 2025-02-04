5bd180609b878f47b346677da36746948c5917b9
halil37 <hllibrahimsartitas@gmail.com>
2025-02-04 20:24:04 +0300

    Iproductrepository.cs modified.

--- src/Eshop/Eshop.Core.Application/Interfaces/Repositories/IProductRepository.cs
+++ src/Eshop/Eshop.Core.Application/Interfaces/Repositories/IProductRepository.cs
@@ -10,7 +10,11 @@ namespace Eshop.Core.Application.Interfaces.Repositories
 {
     public  interface IProductRepository:IRepository<Product>
     {
+	// Return     : CategoryIdProductDto
+	// Parameters : int id, int numberOfProductsPerPage, int currentPage = 1
         Task<CategoryIdProductDto> GetByCatId(int id, int numberOfProductsPerPage, int currentPage = 1);
-        Task<ProductDto> GetPerPageProducts(int numberOfProductsPerPage, int currentPage = 1);
+        //  Return     : ProductDto
+	//  Parameters : int numberOfProductsPerPage, int currentPage = 1
+	Task<ProductDto> GetPerPageProducts(int numberOfProductsPerPage, int currentPage = 1);
     }
 }
